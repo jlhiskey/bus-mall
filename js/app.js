ab98951ec2bf8e0df390ca90fae6802ca7092438
jlhiskey <jasonhi@crazyredhead.com>
2018-07-23 15:15:21 -0700

    js framework

--- js/app.js
+++ js/app.js
@@ -1,2 +1,42 @@
 'use strict';
+//----------Global Variables---------------------------------------------------------------------------------------------------------------
+// Total Products in existence
+// Name of Products
 
+//----------Product Constructor That will give an object their name as well as what type of file they are-------------------------------
+  //I think it will also need to log how many times the product has been looked at so that it doesnt repeat and also keep track of votes for results in the end.
+  //Do I put my splitter that takes file name and separates name from file type inside of the constructor or outside?
+
+//----------Album Function that will take the product names and run them through the product constructor and will rebuild them and turn them into usable things-------------
+
+//----------Voting Machine---------------------------------------------------------------------------------------------------------------------------------
+  // Logs clicks and populates left, center, and right products onto screen-
+
+  //Links to HTML Land
+ 
+  //Calls up random products to display
+  
+  //Needs to make sure that all three objects displayed are unique items that have the least amount of views
+    
+  //Everytime somthing is views keep track
+    
+  //Tells HTML Land what is should be showing
+    // Left
+
+    // Center
+   
+    // Right
+    
+  //Everytime something is clicked keep track
+    //Also needs to keep track of overall clicks so that voting ends at 25 clicks
+  
+  //Create a table at the end of the voting that shows products and total number of votes they have.
+  
+  //Create something that will allow user to click on images.... turn image into button mayhaps???
+  
+    //What happens if button is clicked.
+      //Vote is recorded
+      //25 Votes have occured.
+      //Dumb Dumb didnt click on a picture
+  
+//----------Calling Functions----------------------------------------------------------------------------------------------------------------
